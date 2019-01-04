4b62a2789ebfb7d86c67768c9d178c23556591e6
duffnath <duffnath@msu.edu>
2019-01-04 16:20:14 -0500

    Initial commit

---
+++ MercurySampleApplication/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MercurySampleApplication
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
